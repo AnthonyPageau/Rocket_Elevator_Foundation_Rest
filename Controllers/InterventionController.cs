2a6420206cb9c88a8751d52e7e19ea83941bcec7
Anthony Pageau <antopageau@hotmail.com>
2020-11-17 16:55:21 -0500

    commit for deploy

---
+++ Controllers/InterventionController.cs
@@ -0,0 +1,106 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using RestApi.Models;
+using Newtonsoft.Json.Linq;
+
+namespace RestApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class InterventionController : ControllerBase
+    {
+        private readonly app_developmentContext _context;
+
+        public InterventionController(app_developmentContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Intervention
+        [HttpGet]
+        public ActionResult<List<Intervention>> GetAll ()
+        {
+            var list = _context.interventions.ToList();
+            if (list == null)
+            {
+                return NotFound();
+            }
+            List<Intervention> pending_intervention_list = new List<Intervention>();
+            foreach (var intervention in list)
+            {
+                if (intervention.start_date_and_time_of_the_intervention == null && intervention.status == "Pending") {
+                    pending_intervention_list.Add(intervention);
+                }
+            }
+            return pending_intervention_list;
+        }
+
+        // GET: api/Intervention/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Intervention>> GetIntervention(long id)
+        {
+            var intervention = await _context.interventions.FindAsync(id);
+
+            if (intervention == null)
+            {
+                return NotFound();
+            }
+
+            return intervention;
+        }
+
+        // PUT: api/Intervention/InProgress/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for
+        // more details, see https://go.microsoft.com/fwlink/?linkid=2123754.
+        [HttpPut("InProgress/{id}")]
+        public async Task<IActionResult> InProgresstIntervention(long id, Intervention intervention)
+        {
+           var i = await _context.interventions.FindAsync (id);
+            if (i == null) {
+                return NotFound ();
+           }
+
+
+            DateTime aDate = DateTime.Now;
+            i.status = intervention.status;
+            i.start_date_and_time_of_the_intervention = aDate;
+
+            _context.interventions.Update (i);
+            _context.SaveChanges ();
+
+            // Create a message to show the new status
+            var status = new JObject ();
+            status["message"] = "The status of the Intervention with the id number #" + i.Id + " have been changed to " + i.status;
+            return Content (status.ToString (), "application/json");
+        }
+        // PUT: api/Intervention/Completed/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for
+        // more details, see https://go.microsoft.com/fwlink/?linkid=2123754.
+        [HttpPut("Completed/{id}")]
+        public async Task<IActionResult> CompletedIntervention(long id, Intervention intervention)
+        {
+           var i = await _context.interventions.FindAsync (id);
+            if (i == null) {
+                return NotFound ();
+           }
+
+
+            DateTime aDate = DateTime.Now;
+            i.status = intervention.status;
+            i.end_date_and_time_of_the_intervention = aDate;
+
+            _context.interventions.Update (i);
+            _context.SaveChanges ();
+
+            // Create a message to show the new status
+            var status = new JObject ();
+            status["message"] = "The status of the Intervention with the id number #" + i.Id + " have been changed to " + i.status;
+            return Content (status.ToString (), "application/json");
+        }
+    }
+}
